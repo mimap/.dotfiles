90565d80fb83ffb2cb593ea4a43ae4fa7a363d81
mimap <misham2@gmail.com>
2021-02-26 08:43:19 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sud apt install tmux
 # vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vim/.vimrc $HOME
-cp -r vim/.vim $HOME/.vim
+cp -r vim/.vim/colors $HOME/.vim
 # tmux 
 cp tmux/.tmux.conf $HOME
