797e667135e2c98f61b1e753ed750992015917ac
mimap <misham2@gmail.com>
2021-02-26 08:10:24 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-
+sudo apt install vim
+sud apt install tmux
 # vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vim/.vimrc $HOME
 cp -r vim/.vim $HOME/.vim
 # tmux 
 cp tmux/.tmux.conf $HOME
-# zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"")"
